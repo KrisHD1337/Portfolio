43565b4099752d8c058c7b114f33bfae4c00936b
IM23a-cernik <cernik@bzz.ch>
2025-03-20 10:00:00 +0100

    added dark/light mode toggle

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,38 +1,51 @@
+"use client"
+
 import "./globals.css";
 import Link from "next/link";
+import {ThemeProvider} from "@/components/theme-provider";
+import {ModeToggle} from "@/components/mode-toggle";
+import {useTheme} from "next-themes";
+
 
 export default function RootLayout({children}) {
     return (
-        <html lang="en">
+        <html lang="en" suppressHydrationWarning>
         <body
-            className={"bg-black text-emerald-300"}>
-        <header className={"flex text-5xl p-3 justify-between border-b-2 border-emerald-300"}>
-            <button className={""}>&#9776;</button>
-            <div className={""}>Kristians Portfolio</div>
-            <button className={""}>&#9790;</button>
-        </header>
-        <div className={"p-5"}>
-            {children}
-        </div>
-        <footer className="">
-            <ul className="flex justify-evenly flex-wrap border-t-2 border-emerald-300 p-3">
-                <li className="">
-                    <Link href="/privacy">
-                        Privacy Policy
-                    </Link>
-                </li>
-                <li className="">
-                    <Link href="/contact">
-                        Contact
-                    </Link>
-                </li>
-                <li className="">
-                    <Link href="/about">
-                        About
-                    </Link>
-                </li>
-            </ul>
-        </footer>
+            className={"bg-gray-200 dark:bg-black text-emerald-800 dark:text-emerald-300"}>
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+        >
+            <header className={"flex text-3xl md:text-5xl p-3 justify-between border-b-2 border-emerald-800 dark:border-emerald-300 content-center"}>
+                <button className={""}>&#9776;</button>
+                <div className={"p-2"}>Kristians Portfolio</div>
+                <ModeToggle/>
+            </header>
+            <div className={"p-5"}>
+                {children}
+            </div>
+            <footer className="">
+                <ul className="flex justify-evenly flex-wrap border-t-2 border-emerald-800 dark:border-emerald-300 p-3">
+                    <li className="">
+                        <Link href="/privacy">
+                            Privacy Policy
+                        </Link>
+                    </li>
+                    <li className="">
+                        <Link href="/contact">
+                            Contact
+                        </Link>
+                    </li>
+                    <li className="">
+                        <Link href="/about">
+                            About
+                        </Link>
+                    </li>
+                </ul>
+            </footer>
+        </ThemeProvider>
         </body>
         </html>
     );
---
+++ src/components/mode-toggle.jsx
@@ -0,0 +1,22 @@
+"use client"
+
+import * as React from "react"
+import {Moon, Sun} from "lucide-react"
+import {useTheme} from "next-themes"
+
+import {Button} from "@/components/ui/button"
+
+export function ModeToggle() {
+    const {theme, setTheme} = useTheme()
+
+    return (
+        <Button variant="outline" size="icon" onClick={() =>
+            setTheme(theme === "dark" ? "light" : "dark")}
+                className="bg-gray-300 dark:bg-transparent text-yellow-600 dark:text-yellow-300 lg:scale-150 m-2">
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-125 transition-all dark:-rotate-90 dark:scale-0"/>
+            <Moon
+                className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-125"/>
+            <span className="sr-only">Toggle theme</span>
+        </Button>
+    )
+}
